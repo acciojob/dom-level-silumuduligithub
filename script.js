4483626089c35340bb983e84a511e82b06d31c49
Silu Muduli <130176819+silumuduligithub@users.noreply.github.com>
2023-09-24 12:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let level = document.getElementById("level");
+let count = 0;
+while(level.tagName != "HTML"){
+    level = level.parentNode;
+    count++;
+}
+count++;
+console.log(count);
